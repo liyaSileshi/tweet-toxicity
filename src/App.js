29e36d3bafe85d9fceb727c0f156375c4cf808b3
liyaSileshi <liyasileshi@gmail.com>
2021-01-08 12:48:59 -0800

    predicton working in class based react

--- src/App.js
+++ src/App.js
@@ -1,52 +1,48 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import * as tf from '@tensorflow/tfjs';
 import * as toxicity from '@tensorflow-models/toxicity';
 
+class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      predictions: null,
+      sentence: 'you suck'
+    };
+  }
 
-async function resolveModel() {
-  const threshold = 0.9;
-  // create model
-  return await toxicity.load(threshold);
-  // console.log(model)
-}
-
-async function predict(sentence) {
-  const model = await resolveModel()
-  const classifying = await model.classify([sentence])
-  // console.log(classifying)
-  return classifying
-}
-
-
-function App() {
-  const [predictions, setPredictions] = useState(null);
-  const [sentence, setSentence] = useState('you suck');
-  // The minimum prediction confidence.
-  // const threshold = 0.9;
-  // let model = toxicity.load(threshold);
-
-  const predicts = predict(sentence).then(pre =>
-    // console.log(pre)
-    setPredictions(pre)
-  )
-
-  console.log(predicts)
+  async resolveModel() {
+    const threshold = 0.9;
+    // create model
+    return await toxicity.load(threshold);
+    // console.log(model)
+  }
   
-   
-  return (
-    <div className="App">
+  async predict(sentence) {
+    const model = await this.resolveModel()
+    const classifying = await model.classify([sentence])
+    this.setState({
+      predictions: classifying[6].results[0].probabilities[0]
+    })
+    return classifying[6].results[0].probabilities[0]
+  }
+
+  render() {
+    return (
+      <div className="App">
       <p>What is your terrible sentence?</p>
       <input 
         type="text" 
-        value={sentence} 
-        onChange={(e) => setSentence(e.target.value)} 
+        value={this.state.sentence} 
+        onChange={(e) => this.setState({sentence: e.target.value})} 
       />
-      <button onClick={predict}>Submit</button>
-      Toxicity likelihood: 0
+      <button onClick={() => this.predict(this.state.sentence)}>Submit</button>
+      Toxicity likelihood: {this.state.predictions}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
