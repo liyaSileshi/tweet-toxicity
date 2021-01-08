237040c72e57b36c754766e403469d6812f3177d
liyaSileshi <liyasileshi@gmail.com>
2021-01-08 12:25:46 -0800

    error on predict GPU

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as toxicity from '@tensorflow-models/toxicity';
+
+
+
+async function resolveModel() {
+  const threshold = 0.9;
+  // create model
+  return await toxicity.load(threshold);
+  // console.log(model)
+}
+
+async function predict(sentence) {
+  const model = await resolveModel()
+  const classifying = await model.classify([sentence])
+  // console.log(classifying)
+  return classifying
+}
+
 
 function App() {
+  const [predictions, setPredictions] = useState(null);
+  const [sentence, setSentence] = useState('you suck');
+  // The minimum prediction confidence.
+  // const threshold = 0.9;
+  // let model = toxicity.load(threshold);
+
+  const predicts = predict(sentence).then(pre =>
+    // console.log(pre)
+    setPredictions(pre)
+  )
+
+  console.log(predicts)
+  
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>What is your terrible sentence?</p>
+      <input 
+        type="text" 
+        value={sentence} 
+        onChange={(e) => setSentence(e.target.value)} 
+      />
+      <button onClick={predict}>Submit</button>
+      Toxicity likelihood: 0
     </div>
   );
 }
